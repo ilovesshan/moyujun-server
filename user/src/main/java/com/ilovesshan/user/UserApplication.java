26b19208cb018a29ca4d4bc4fe46af88fe00e041
ilovesshan <ilovesshany@gmail.com>
2023-07-29 17:52:28 +0800

    init project and add user model dependencies

---
+++ user/src/main/java/com/ilovesshan/user/UserApplication.java
@@ -0,0 +1,18 @@
+package com.ilovesshan.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2023/7/29
+ * @description:
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
