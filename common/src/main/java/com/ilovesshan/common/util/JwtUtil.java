952b0444e27ee9910c2b52be521d9f972072e131
ilovesshan <ilovesshany@gmail.com>
2023-08-02 22:34:27 +0800

    feat: add token util

---
+++ common/src/main/java/com/ilovesshan/common/util/JwtUtil.java
@@ -0,0 +1,59 @@
+package com.ilovesshan.common.util;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2023/8/2
+ * @description:
+ */
+
+public class JwtUtil {
+
+    private static final String JWT_KEY = "EZzfm1n3joVCNsJl";
+    private static final long JWT_EXPIRATION = 1000 * 60 * 60 * 24L;
+
+    // 生成token+
+    public static String generatorToken(String userId, String username) {
+        Claims claims = new DefaultClaims();
+        claims.put("userId", userId);
+        claims.put("username", username);
+        return Jwts.builder()
+                .setId(userId)
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, JWT_KEY.getBytes())
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .compact();
+    }
+
+    // 获取Claims对象
+    public static Claims parseToken(String token) {
+        Claims claims = null;
+        try {
+            claims = Jwts.parser().setSigningKey(JWT_KEY.getBytes()).parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return claims;
+    }
+
+    // 获取用户Id
+    public static String getUserId(String token) {
+        return (String) parseToken(token).get("userId");
+    }
+
+
+    // 获取用户名
+    public static String getUsername(String token) {
+        return (String) parseToken(token).get("username");
+    }
+}
